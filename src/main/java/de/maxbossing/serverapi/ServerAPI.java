51178ede678d2c855c4dc813260ba0571d3ffc38
maxbossing <info@maxbossing.de>
2022-11-16 17:06:35 +0100

    added VERSION constant

--- src/main/java/de/maxbossing/serverapi/ServerAPI.java
+++ src/main/java/de/maxbossing/serverapi/ServerAPI.java
@@ -1,5 +1,6 @@
 package de.maxbossing.serverapi;
 
+import de.maxbossing.serverapi.ItemUtils.SkullBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,11 +8,12 @@ public final class ServerAPI extends JavaPlugin {
 
     private static ServerAPI instance;
 
+    public static final String VERISON() {return "1.2.1";}
+
     @Override
     public void onEnable() {
     instance = this;
-    Bukkit.getServer().getConsoleSender().sendMessage("[ServerAPI] ServerAPI Loaded");
-    }
+    Bukkit.getServer().getConsoleSender().sendMessage("[ServerAPI] ServerAPI Loaded");}
 
     @Override
     public void onDisable() {
